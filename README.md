42db789597738b40be49aa06d012d80bdeece6b3
Kyrylo Sovailo <k.sovailo@gmail.com>
2024-10-03 22:18:53 +0200

    Added Debian style

---
+++ README.md
@@ -0,0 +1,3 @@
+### PS1
+
+Small collection of `PS1` values I came by -- including ones that print git branches.
--- source.sh
+++ source.sh
@@ -1,25 +1,61 @@
 #!/bin/sh
 
-COLOR_FG_BLACK="\[\e[01;30m\]"
-COLOR_FG_RED="\[\e[01;31m\]"
-COLOR_FG_GREEN="\[\e[01;32m\]"
-COLOR_FG_YELLOW="\[\e[01;33m\]"
-COLOR_FG_BLUE="\[\e[01;34m\]"
-COLOR_FG_MAGENTA="\[\e[01;35m\]"
-COLOR_FG_CYAN="\[\e[01;36m\]"
-COLOR_FG_WHITE="\[\e[01;37m\]"
-COLOR_FG_BRIGHT_BLACK="\[\e[01;90m\]"
-COLOR_FG_BRIGHT_RED="\[\e[01;91m\]"
-COLOR_FG_BRIGHT_GREEN="\[\e[01;92m\]"
-COLOR_FG_BRIGHT_YELLOW="\[\e[01;93m\]"
-COLOR_FG_BRIGHT_BLUE="\[\e[01;94m\]"
-COLOR_FG_BRIGHT_MAGENTA="\[\e[01;95m\]"
-COLOR_FG_BRIGHT_CYAN="\[\e[01;96m\]"
-COLOR_FG_BRIGHT_WHITE="\[\e[01;97m\]"
-COLOR_FG_DEFAULT="\[\e[00m\]"
+COLOR_FG_BLACK="\033[01;30m"
+COLOR_FG_RED="\033[01;31m"
+COLOR_FG_GREEN="\033[01;32m"
+COLOR_FG_YELLOW="\033[01;33m"
+COLOR_FG_BLUE="\033[01;34m"
+COLOR_FG_MAGENTA="\033[01;35m"
+COLOR_FG_CYAN="\033[01;36m"
+COLOR_FG_WHITE="\033[01;37m"
+COLOR_FG_BRIGHT_BLACK="\033[01;90m"
+COLOR_FG_BRIGHT_RED="\033[01;91m"
+COLOR_FG_BRIGHT_GREEN="\033[01;92m"
+COLOR_FG_BRIGHT_YELLOW="\033[01;93m"
+COLOR_FG_BRIGHT_BLUE="\033[01;94m"
+COLOR_FG_BRIGHT_MAGENTA="\033[01;95m"
+COLOR_FG_BRIGHT_CYAN="\033[01;96m"
+COLOR_FG_BRIGHT_WHITE="\033[01;97m"
+COLOR_FG_DEFAULT="\033[m"
 
-# Debian style
-export PS1="${COLOR_FG_GREEN}\u@\h${COLOR_FG_BLUE} \w \$${COLOR_FG_DEFAULT} "
+append_git_branch() {
+    BRANCH=$(git branch --show-current 2>/dev/null)
+    if [ $? == 0 ]; then
+        printf "${COLOR_FG_YELLOW}${BRANCH}${COLOR_FG_BLUE} "
+    fi
+}
 
-# Debian style (root)
-export PS1="${COLOR_FG_RED}\h${COLOR_FG_BLUE} \w \$${COLOR_FG_DEFAULT} "
+append_debian_git_branch() {
+    BRANCH=$(git branch --show-current 2>/dev/null)
+    if [ $? == 0 ]; then
+        printf "${COLOR_FG_DEFAULT}:${COLOR_FG_YELLOW}${BRANCH}${COLOR_FG_BLUE}"
+    fi
+}
+
+append_debian_root_git_branch() {
+    BRANCH=$(git branch --show-current 2>/dev/null)
+    if [ $? == 0 ]; then
+        printf ":${BRANCH}"
+    fi
+}
+
+PS1_STYLE="gentoo-git"
+if [ "${PS1_STYLE}" == "gentoo" ]; then
+    export PS1="${COLOR_FG_GREEN}\u@\h${COLOR_FG_BLUE} \w \$${COLOR_FG_DEFAULT} "
+elif [ "${PS1_STYLE}" == "gentoo-root" ]; then
+    export PS1="${COLOR_FG_RED}\h${COLOR_FG_BLUE} \w \$${COLOR_FG_DEFAULT} "
+elif [ "${PS1_STYLE}" == "gentoo-git" ]; then
+    export PS1="${COLOR_FG_GREEN}\u@\h${COLOR_FG_BLUE} \w "'$(append_git_branch)'"\$${COLOR_FG_DEFAULT} "
+elif [ "${PS1_STYLE}" == "gentoo-root-git" ]; then
+    export PS1="${COLOR_FG_RED}\h${COLOR_FG_BLUE} \w "'$(append_git_branch)'"\$${COLOR_FG_DEFAULT} "
+elif [ "${PS1_STYLE}" == "debian" ]; then
+    export PS1="${COLOR_FG_GREEN}\u@\h${COLOR_FG_DEFAULT}:${COLOR_FG_BLUE}\w${COLOR_FG_DEFAULT}\$ "
+elif [ "${PS1_STYLE}" == "debian-root" ]; then
+    export PS1="\u@\h:\w\$ "
+elif [ "${PS1_STYLE}" == "debian-git" ]; then
+    export PS1="${COLOR_FG_GREEN}\u@\h${COLOR_FG_DEFAULT}:${COLOR_FG_BLUE}\w"'$(append_debian_git_branch)'"${COLOR_FG_DEFAULT}\$ "
+elif [ "${PS1_STYLE}" == "debian-root-git" ]; then
+    export PS1="\u@\h:\w"'$(append_debian_root_git_branch)'"\$ "
+else
+    export PS1="\u@\h:\w\$ "
+fi
